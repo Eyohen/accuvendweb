00860aa27753272981431e28b408236c8006e85c
Joshua <cutejosh2@gmail.com>
2023-11-15 23:35:51 +0100

    fix: navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,11 +4,7 @@ import { HiBars3 } from "react-icons/hi2";
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
-    const [showMobileMenu, setShowMobileMenu] = useState(false);
-
-    const toggleMobileMenu = () => {
-        setShowMobileMenu(!showMobileMenu);
-    };
+    const [mobileNavOpen, setMobileNavOpen] = useState(false)
 
     return (
         <div className="">
@@ -17,14 +13,12 @@ const Navbar = () => {
                     <Link to="/">
                         <img src={Logo} alt="Logo" className="h-14 cursor-pointer" />
                     </Link>
-                    <span className="text-3xl cursor-pointer mx-2 md:hidden block" onClick={toggleMobileMenu}>
+                    <button onClick={() => setMobileNavOpen((prev) => !prev)} className="text-3xl cursor-pointer mx-2 md:hidden block">
                         <HiBars3 />
-                    </span>
+                    </button>
                 </div>
 
-                <ul className={`md:flex md:items-center z-10 md:static absolute bg-white w-full left-0 
-                    md:w-auto md:py-0 py-4 md:pl-0 pl-7 ${showMobileMenu ? 'opacity-100 top-20' : 'opacity-0 top-[-400px]'} 
-                    transition-all ease-in duration-500 md:space-x-4 text-sm`}>
+                <ul className="md:flex md:items-center space-x-4 hidden">
                     <li>
                         <Link to='/' className="text-xl hover:text-[#002E87] duration-500 font-medium">Home</Link>
                     </li>
@@ -36,6 +30,23 @@ const Navbar = () => {
                     </li>
                 </ul>
             </nav>
+            {mobileNavOpen && (
+                <nav className="bg-white flex-col space-y-2 w-full z-20 px-8 pt-2 pb-4 md:hidden">
+                    <hr />
+                    <div>
+                        <Link to='/' className="text-xl hover:text-[#002E87] duration-500 font-medium">Home</Link>
+                    </div>
+                    <hr />
+                    <div>
+                        <Link to='/about' className="text-xl hover:text-[#002E87] duration-500 font-medium">About Us</Link>
+                    </div>
+                    <hr />
+                    <div>
+                        <Link to='/contact' className="text-xl hover:text-[#002E87] duration-500 font-medium">Contact Us</Link>
+                    </div>
+                    <hr />
+                </nav>
+            )}
         </div>
     );
 };
